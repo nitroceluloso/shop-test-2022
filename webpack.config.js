9d9db3b98336e4005a85420dccca97fcc32e70f0
Cristóbal <cristobalsepulveda1@hotmail.com>
2022-06-15 11:37:29 -0400

    build: setting up basic configuration.

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.tsx',
+    devtool: 'inline-source-map',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ]
+    },
+    resolve: {
+        extensions: [ '.tsx', '.ts', '.js' ],
+        alias: {
+            '@Utils': path.resolve(__dirname, './src/utils/'),
+            '@Components': path.resolve(__dirname, './src/components/')
+        }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({ template: './src//assets/index.html' })
+    ],
+    devServer:{
+        open: true,
+        liveReload: true,
+        compress: true,
+        port: 9000,
+        static: {
+            directory: './dist'
+        },
+        client: {
+            // logging: 'info'
+        }
+    }
+}
