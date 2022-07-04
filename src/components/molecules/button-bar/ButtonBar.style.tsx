4dd5acb9229d5e9f1c46f38561dc785b6f77f6c1
Cristóbal <cristobalsepulveda1@hotmail.com>
2022-07-04 13:34:59 -0400

    feature: added button bar.

--- src/components/molecules/button-bar/ButtonBar.style.tsx
+++ src/components/molecules/button-bar/ButtonBar.style.tsx
@@ -1,16 +1,22 @@
 import styled from 'styled-components';
 
-export const ButtonBarContainer = styled.div`
+type ButtonBarContainerProps = {
+    transparent?: boolean;
+}
+
+export const ButtonBarContainer = styled.div<ButtonBarContainerProps>`
     width: 100%;
     position: fixed;
     bottom: 0;
     padding:  0 1rem 1rem 1rem;
+    opacity: ${props => props.transparent ? '0.2' : 1};
 `;
 
 export const ButtonContainer = styled.div`
     padding-top: 1rem;
     display: flex;
     justify-content: space-between;
+    flex-direction: row-reverse;
 `;
 
 export const OptionButtonBox = styled.div`
--- src/components/molecules/button-bar/ButtonBar.tsx
+++ src/components/molecules/button-bar/ButtonBar.tsx
@@ -1,17 +1,34 @@
 import React from 'react';
 import Button from '@Components/atoms/button';
 import { ButtonBarContainer, Separation, ButtonContainer, OptionButtonBox } from './ButtonBar.style';
+import Plus from "@Assets/svg/plus.svg";
+import Trash from "@Assets/svg/trash.svg";
+import Share from "@Assets/svg/share.svg";
 
-export const ButtonBar = () => {
+type ButtonBarProps = {
+    showOptions: boolean;
+}
+
+export const ButtonBar = (props: ButtonBarProps) => {
     return (
         <ButtonBarContainer>
             <Separation/>
             <ButtonContainer>
-                <OptionButtonBox>
-                    <Button title='+' look='option' onClick={() => {}} />
-                    <Button title='+' look='option' onClick={() => {}} />
-                </OptionButtonBox>
-                <Button title='+' look='action' onClick={() => {}} />
+                <Button look='action' onClick={() => {}}>
+                    <img src={Plus} alt="" style={{height: '21px'}}/>
+                </Button>
+
+                {
+                    props.showOptions &&
+                    <OptionButtonBox>
+                        <Button look='option' onClick={() => {}}>
+                            <img src={Trash} alt="" style={{height: '21px'}}/>
+                        </Button>
+                        <Button look='option' onClick={() => {}} >
+                            <img src={Share} alt="" style={{height: '21px'}}/>
+                        </Button>
+                    </OptionButtonBox>
+                }
             </ButtonContainer>
         </ButtonBarContainer>
     );
